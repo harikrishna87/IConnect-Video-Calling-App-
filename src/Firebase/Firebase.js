ca4a5dba27f4a72d7b84dfa3d6832de2249d27e6
harikrishna87 <krishnahari5048@gmail.com>
2025-03-10 17:55:59 +0530

    IConnect

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -14,12 +14,12 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey:process.env.VITE_APIKEY,
+  authDomain: process.env.VITE_AUTHDOMAIN,
+  projectId: process.env.VITE_PROJECTID,
+  storageBucket: process.env.VITE_STORAGEBUCKET,
+  messagingSenderId: process.env.VITE_MESSAGINGSENDERID,
+  appId: process.env.VITE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
